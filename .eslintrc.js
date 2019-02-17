ffa52e86217456505106196135eeb18a8f275ceb
Wagner <jaykob.wagner@gmail.com>
2019-02-17 10:00:31 +0100

    initialize eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        // enable additional rules
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "windows"],
+        "no-console": "off",
+        "global-require": "off",
+        "prefer-destructuring": "off",
+        "import/no-dynamic-require": "off"
+        //"class-methods-use-this": ["error", "disable"]
+        }
+};
