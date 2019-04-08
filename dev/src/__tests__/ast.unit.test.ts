653d86e0452f78f5995af9b87ecf88e78c71c63e
Wagner <jaykob.wagner@gmail.com>
2019-04-08 08:47:03 +0200

    fix ast unit test

--- dev/src/__tests__/ast.unit.test.ts
+++ dev/src/__tests__/ast.unit.test.ts
@@ -1,13 +1,23 @@
 import { Token } from 'zion-lexer';
 import { Ast } from '../Ast';
 import { ParseError } from '../ErrorHandler/parse-error';
+import * as Nodes from '../nodes';
 import { Node } from '../nodes';
 
 test('Ast Constructor works expected', () => {
   const nodes: Node[] = [];
   const tokens: Token[] = [];
   const errors: ParseError[] = [];
-  nodes.push(new Node(1, 2, 3));
+  nodes.push(
+    new Nodes.RecordingModeClause(
+      {
+        startColumnTotal: 1,
+        startColumnRelative: 2,
+        startLine: 3,
+      },
+      'V',
+    ),
+  );
   tokens.push(new Token());
   errors.push(new ParseError('msg', 1, 2, 3, 'desc'));
 
